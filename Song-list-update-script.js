30140d50acbc4ab1ad464947543decc2b6f1e652
Christopher Wong <c66wong@uwaterloo.ca>
2026-04-16 18:08:43 -0400

    Update upload workflow and add song list generation script

---
+++ Song-list-update-script.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+const audioDir = path.join(__dirname, '../Audio')
+const outputFile = path.join(__dirname, '../Song-list.json')
+
+const files = fs.readdirSync(audioDir)
+
+const songs = files
+  .filter(file => file.endsWith('.mp3'))
+  .map(file => {
+    const base = file
+      .replace('.mp3', '')
+      .replace(/^\d+\s*/, '') // removes "130 "
+
+    return {
+      id: base,
+      title: base,
+      file: `Audio/${file}`,
+      cover: `Thumbnail/${base}.jpg`,
+      preload: "none"
+    }
+  })
+
+fs.writeFileSync(outputFile, JSON.stringify(songs, null, 2))
+
+console.log("Song-list.json generated!")
