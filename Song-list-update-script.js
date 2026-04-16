a027a54dfe2a8e5e8f0dfc86ac83e3c7656040ca
Christopher Wong <c66wong@uwaterloo.ca>
2026-04-16 19:57:35 -0400

    Refactor song list generation to include shuffling of songs

--- Song-list-update-script.js
+++ Song-list-update-script.js
@@ -6,21 +6,31 @@ const outputFile = path.join(__dirname, 'Song-list.json')
 
 const files = fs.readdirSync(audioDir)
 
-const songs = files
-  .filter(file => file.endsWith('.mp3'))
-  .map(file => {
-    const base = file
-      .replace('.mp3', '')
-      .replace(/^\d+\s*/, '') // removes "130 "
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
 
-    return {
-      id: base,
-      title: base,
-      file: `Audio/${file}`,
-      cover: `Thumbnail/${base}.jpg`,
-      preload: "none"
-    }
-  })
+const songs = shuffle(
+    files
+        .filter(file => file.endsWith('.mp3'))
+        .map(file => {
+            const base = file
+                .replace('.mp3', '')
+                .replace(/^\d+\s*/, '') // removes "130 "
+
+            return {
+                id: base,
+                title: base,
+                file: `Audio/${file}`,
+                cover: `Thumbnail/${base}.jpg`,
+                preload: "none"
+            }
+        })
+)
 
 fs.writeFileSync(outputFile, JSON.stringify(songs, null, 2))
 
